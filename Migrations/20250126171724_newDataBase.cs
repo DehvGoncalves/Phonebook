602b5a4e3b9343523bc8b8c493340a46b5ea583e
Dehv <71988020+DehvGoncalves@users.noreply.github.com>
2025-01-26 15:08:36 -0300

    Add files via upload

---
+++ Migrations/20250126171724_newDataBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeuSiteEmMVC.Migrations
+{
+    public partial class newDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
